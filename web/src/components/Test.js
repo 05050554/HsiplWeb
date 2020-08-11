da947c445e2f1e1268e4941123246955719bfb72
05050554 <jason5366831@gmail.com>
2020-08-11 18:01:39 +0800

    First

---
+++ web/src/components/Test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Test.css";
+
+export const Test = () => {
+    return (
+        <div className="test1">
+            <h1>123</h1>
+        </div>
+    );
+};
+
