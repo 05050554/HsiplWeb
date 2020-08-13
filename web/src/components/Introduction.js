514f746408400a94c52cadf6b8ed65e659f96f80
Mei-Yun-W <bear70118@gmail.com>
2020-08-13 16:13:59 +0800

    Introduction完成

---
+++ web/src/components/Introduction.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./Introduction.css"
+
+const Introduction = () => {
+    return (
+        <div className="Introduction">
+            
+            <div className="videobox">
+                <h1>Introduction</h1>
+                <video autoPlay={'autoPlay'} preload={'none'} controls={'controls'} >
+                        <source src={'https://video.wixstatic.com/video/096150_1449a5a33d164b1a9b2a81f95b734c82/1080p/mp4/file.mp4'} type="video/mp4" />
+                </video>
+            </div>
+            <div className="text">
+                <span>The Hyperspectral Signal and Image Processing Laboratory (HSIPL), 
+                    founded in Sept. 2014, is under the direction of Dr. Shih-Yu Chen. 
+                    HSIPL is dedicated to design and develop algorithms for multi/hyperspectral, 
+                    medical and satellite images, specifically for image classification, 
+                    endmember finding and target detection. In particular, 
+                    we have wide interests in various research topics for multi/hyperspectral applications.
+                </span>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Introduction;
